9fac3306e21f8b6a870f7f585e0429e09b10da14
Subhash Edakkuda <subhash.ep@gmail.com>
2019-11-20 16:32:52 +0530

    Added via upload to master branch
    Added all files

---
+++ Controllers/TracksController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JenkinsCRUD.Models;
+
+namespace JenkinsCRUD.Controllers
+{
+    public class TracksController : Controller
+    {
+        private readonly TrackContext _context;
+
+        public TracksController(TrackContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tracks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Track.ToListAsync());
+        }
+
+        // GET: Tracks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var track = await _context.Track
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (track == null)
+            {
+                return NotFound();
+            }
+
+            return View(track);
+        }
+
+        // GET: Tracks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tracks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Artist,Price,Genre")] Track track)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(track);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(track);
+        }
+
+        // GET: Tracks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var track = await _context.Track.FindAsync(id);
+            if (track == null)
+            {
+                return NotFound();
+            }
+            return View(track);
+        }
+
+        // POST: Tracks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Artist,Price,Genre")] Track track)
+        {
+            if (id != track.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(track);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TrackExists(track.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(track);
+        }
+
+        // GET: Tracks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var track = await _context.Track
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (track == null)
+            {
+                return NotFound();
+            }
+
+            return View(track);
+        }
+
+        // POST: Tracks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var track = await _context.Track.FindAsync(id);
+            _context.Track.Remove(track);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TrackExists(int id)
+        {
+            return _context.Track.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/TrackContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JenkinsCRUD.Models;
+
+    public class TrackContext : DbContext
+    {
+        public TrackContext (DbContextOptions<TrackContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Track> Track { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Track>().ToTable("Track");
+    }
+}
